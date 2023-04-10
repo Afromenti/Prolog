41651163730ba84c4d9124ffeb47707c22564e58
Jakub Krać <129058479+Afromenti@users.noreply.github.com>
2023-04-10 16:59:10 +0200

    Create Zadanie1_1.pl

---
+++ Zadanie1_1.pl
@@ -0,0 +1,18 @@
+/*
+A) Są rodzeństwem
+B) Są kuzynami
+C) Są kuzynami
+D) Ciocią lub wujkiem (X dla Y) 
+E) wnukiem (X Dla Y)
+F) prapra wnukiem (X dla Y)
+G) wnukiem (X dla Y)
+
+*/
+
+rodzenstwo(krzys,malgosia).
+rodzenstwo(malgosia,krzys).
+kuzyn(ala,marek).
+kuzyn(marek,ala).
+ciocialubwujek(stefan,ala).
+wnuk(marcin,ela).
+praprawnuk(krzys,tomasz).
